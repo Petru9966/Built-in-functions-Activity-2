19a628939e2bf68d0720ca232724ca76fc07bc67
Replit user <replituser@example.com>
2021-03-13 07:09:40 +0000

    retrieving a character from a given ASCII code using in range fa given ASCII code using in range built in function

---
+++ main.py
@@ -0,0 +1,11 @@
+# initiate program
+print("\n\t\t\tProgram initiated!")
+#ask user to enter a valid ascii code
+print("\n Please enter a valid ASCII code")
+#define user code as an input
+code=int(input())
+if code in range(32,126):
+  print("The correspondent letter for",code," ASCII code  is","\"", chr(code) ,"\"")
+else:
+  print("This is not a printable character!")
+print("\n\t\t\tProgram finished!")
