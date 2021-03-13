65891f4eb4ab024e05f9e2e571cd81cd81c59d84
Replit user <replituser@example.com>
2021-03-13 07:18:28 +0000

    Minor cosmetical modifications added.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print("\n Please enter a valid ASCII code")
 #define user code as an input
 code=int(input())
 if code in range(32,126):
-  print("The correspondent letter for",code," ASCII code  is","\"", chr(code) ,"\"")
+  print("The correspondent letter for \"",code,"\"""ASCII code is" "\"", chr(code) ,"\"")
 else:
   print("This is not a printable character!")
 print("\n\t\t\tProgram finished!")
